795b12f2d60639ead40e1ead8755067356247c0b
Jennifer B Jones <66266330+jenjones319@users.noreply.github.com>
2020-08-26 15:57:16 -0500

    added console project solution prework

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oNotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
